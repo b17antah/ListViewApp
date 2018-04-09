bdee247cce44b3d061ac4ebf169d73ff2d4f1cb7
b17antah <b17antah@student.his.se>
2018-04-09 11:57:03 +0200

    Toast corrected

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Toast.makeText(getApplicationContext(), "Alps", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), mountainNames[position]+" is in the "+mountainLocations[position]+" category and has a height of "+Integer.toString(mountainHeights[position])+"m", Toast.LENGTH_LONG).show();
             }
         });
 
